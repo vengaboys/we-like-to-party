d9edd4ee41c983a91d074c26ce685ee1f5f353b7
alexbaeza <42570659+alexbaeza@users.noreply.github.com>
2023-08-10 13:17:03 +0100

    Add cloudflare api token encrypted with age, add caddy dns cloudflare configuration for automated tls with cloudflare using dns records

--- secrets.nix
+++ secrets.nix
@@ -1,15 +1,13 @@
 let
-  users = [
-    # chris
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOUR/dExxJt7KpoYoqSpEb1unetXjI47yQpS5cFH51hM"
+  chris = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOUR/dExxJt7KpoYoqSpEb1unetXjI47yQpS5cFH51hM";
+  martin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINc/DDmsDE+KUR1xquEBGIoKbPgLwCbL315XMFP2/XSn";
+  alex = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGXjEARc950hpmlCZmFzpjJJ/8WtrnIZxKO3LkQRQYCK";
 
-    # martin
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINc/DDmsDE+KUR1xquEBGIoKbPgLwCbL315XMFP2/XSn"
+  users = [ chris martin alex ];
 
-    # alex
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGXjEARc950hpmlCZmFzpjJJ/8WtrnIZxKO3LkQRQYCK"
-  ];
-in 
+  kimDeployer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/hujpdZEq6dC53eYfJgOIKOKWKrfjBGv7Hl754js/i";
+in
 {
   "secrets/kim-private-key.age".publicKeys = users;
+  "secrets/alex_cloudflare_api_token.age".publicKeys = [kimDeployer alex];
 }
--- services/caddy.nix
+++ services/caddy.nix
@@ -1,11 +1,16 @@
 { config, pkgs, lib, modulesPath, ... }:
-{
+let
+  alex_cloudflare_api_key = builtins.readFile config.age.secrets.alex_cloudflare_api_token.path;
+in {
   services.caddy = {
     enable = true;
     email = "vengaboys-dev@example.com";
 
     virtualHosts."mediumrare.ai" = {
         extraConfig = ''
+          tls {
+            dns cloudflare ${alex_cloudflare_api_key}
+          }
           reverse_proxy localhost:4000
           encode gzip
           header / {
