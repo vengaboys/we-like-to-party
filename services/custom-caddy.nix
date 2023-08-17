7882dd26ab5f3ef76904aa9e1409da93d334ee5a
alexbaeza <42570659+alexbaeza@users.noreply.github.com>
2023-08-17 10:19:14 +0100

    Use version 2.5.1 with buildGo117Module

--- services/custom-caddy.nix
+++ services/custom-caddy.nix
@@ -1,100 +1,72 @@
-{ stdenv
-, lib
-, buildGoModule
-, fetchFromGitHub
-, nixosTests
-, caddy
-, testers
-, installShellFiles
-, plugins ? []
-, vendorSha256 ? ""
-}:
-
-with lib;
-
+{ lib, buildGo117Module, fetchFromGitHub, nixosTests, plugins ? [ ], pkgs }:
 let
-  version = "2.7.3";
+  version = "2.5.1";
   dist = fetchFromGitHub {
     owner = "caddyserver";
     repo = "dist";
     rev = "v${version}";
-    hash = "sha256-fQhujBYyl2p3cdqyf+LVebPBGxXn2lKMB/dsvTo5+NM=";
+    sha256 = "sha256:1nlphjg5wh5drpwkm4cczrkxdzbv72ll7hp5x7z6ww8pzz3q10b3";
   };
-  imports = flip concatMapStrings plugins (pkg: "\t\t\t_ \"${pkg}\"\n");
-	main = ''
-		package main
+  imports = lib.flip lib.concatMapStrings plugins (pkg: "	_ \"${pkg}\"\n");
+  main = ''
+    package main
 
-		import (
-			caddycmd "github.com/caddyserver/caddy/v2/cmd"
+    import (
+    	caddycmd "github.com/caddyserver/caddy/v2/cmd"
+    	_ "github.com/caddyserver/caddy/v2/modules/standard"
+    ${imports}
+    )
 
-			_ "github.com/caddyserver/caddy/v2/modules/standard"
-${imports}
-		)
-
-		func main() {
-			caddycmd.Main()
-		}
-	'';
-in
-buildGoModule {
+    func main() {
+    	caddycmd.Main()
+    }
+  '';
+in buildGo117Module {
   pname = "caddy";
   inherit version;
+  runVend = true;
+
+  subPackages = [ "cmd/caddy" ];
 
   src = fetchFromGitHub {
     owner = "caddyserver";
     repo = "caddy";
     rev = "v${version}";
-    hash = "sha256-KezKMpx3M7rdKXEWf5XUSXqY5SEimACkv3OB/4XccCE=";
+    sha256 = "sha256:1nlphjg5wh5drpwkm4cczrkxdzbv72ll7hp5x7z6ww8pzz3q10b3";
   };
 
-  vendorHash = "sha256-mTHEM+0yakKiy4ZFi+2qakjSlKFyRkbjeXOXdvx+9lA=";
+  vendorSha256 = "sha256:082yh6rqr41xwg2xpqg2sdms9m7bw68bc44bgg3xwfbgdbzrs0ni";
 
-  subPackages = [ "cmd/caddy" ];
+  nativeBuildInputs = [ pkgs.breakpointHook ];
 
-  ldflags = [
-    "-s" "-w"
-    "-X github.com/caddyserver/caddy/v2.CustomVersion=${version}"
-  ];
+  overrideModAttrs = (_: {
+    preBuild = "echo '${main}' > cmd/caddy/main.go";
+    postInstall = "cp go.sum go.mod $out/ && ls $out/";
+  });
 
-  nativeBuildInputs = [ installShellFiles ];
+  postPatch = ''
+    echo '${main}' > cmd/caddy/main.go
+    cat cmd/caddy/main.go
+  '';
 
-	overrideModAttrs = (_: {
-		preBuild    = "echo '${main}' > cmd/caddy/main.go";
-		postInstall = "cp go.sum go.mod $out/ && ls $out/";
-	});
-
-	postPatch = ''
-		echo '${main}' > cmd/caddy/main.go
-		cat cmd/caddy/main.go
-	'';
+  postConfigure = ''
+    cp vendor/go.sum ./
+    cp vendor/go.mod ./
+  '';
 
   postInstall = ''
     install -Dm644 ${dist}/init/caddy.service ${dist}/init/caddy-api.service -t $out/lib/systemd/system
 
     substituteInPlace $out/lib/systemd/system/caddy.service --replace "/usr/bin/caddy" "$out/bin/caddy"
     substituteInPlace $out/lib/systemd/system/caddy-api.service --replace "/usr/bin/caddy" "$out/bin/caddy"
-
-    $out/bin/caddy manpage --directory manpages
-    installManPage manpages/*
-
-    installShellCompletion --cmd caddy \
-      --bash <($out/bin/caddy completion bash) \
-      --fish <($out/bin/caddy completion fish) \
-      --zsh <($out/bin/caddy completion zsh)
   '';
 
-  passthru.tests = {
-    inherit (nixosTests) caddy;
-    version = testers.testVersion {
-      command = "${caddy}/bin/caddy version";
-      package = caddy;
-    };
-  };
+  passthru.tests = { inherit (nixosTests) caddy; };
 
   meta = with lib; {
     homepage = "https://caddyserver.com";
-    description = "Fast and extensible multi-platform HTTP/1-2-3 web server with automatic HTTPS";
+    description = "Fast, cross-platform HTTP/2 web server with automatic HTTPS";
     license = licenses.asl20;
-    maintainers = with maintainers; [ Br1ght0ne emilylange techknowlogick ];
+    maintainers = with maintainers; [ Br1ght0ne techknowlogick ];
   };
 }
